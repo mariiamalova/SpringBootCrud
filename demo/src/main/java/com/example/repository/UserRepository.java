3ac25894baf37c2156410de0261f6750b82030f9
mariiamalova <106603539+mariiamalova@users.noreply.github.com>
2022-06-20 22:18:48 +0300

    Add files via upload

---
+++ demo/src/main/java/com/example/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.repository;
+
+
+import java.util.List;
+
+import com.example.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
